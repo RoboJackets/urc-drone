1e5581a50a839a493ea4ed07ff73db472cecb7a4
a-stickan <aidan.stickan@gmail.com>
2022-10-23 16:29:19 -0400

    Linting fixes

--- urc_gazebo/launch/simulation.launch.py
+++ urc_gazebo/launch/simulation.launch.py
@@ -1,9 +1,9 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+# from launch.substitutions import PathJoinSubstitution
+# from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
+# from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -20,7 +20,7 @@ def generate_launch_description():
         ),
         launch_arguments={"world": world_path}.items()
     )
-    
+
     drone = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_urc_gazebo, "launch", "spawn_drone.py")
