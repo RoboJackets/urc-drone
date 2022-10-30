c4be9ca7166a1215538aea65ff0cadbce797390b
a-stickan <aidan.stickan@gmail.com>
2022-10-30 16:55:24 -0400

    Fixed linting errors

--- urc_aruco/include/aruco_detector.hpp
+++ urc_aruco/include/aruco_detector.hpp
@@ -20,30 +20,31 @@ namespace aruco_detector
 class ArucoDetector : public rclcpp::Node
 {
 public:
-    explicit ArucoDetector(const rclcpp::NodeOptions & options);
+  explicit ArucoDetector(const rclcpp::NodeOptions & options);
 
 private:
-    rclcpp::Publisher<urc_msgs::msg::ArucoDetection>::SharedPtr aruco_publisher;
-    image_transport::CameraSubscriber camera_subscriber_;
+  rclcpp::Publisher<urc_msgs::msg::ArucoDetection>::SharedPtr aruco_publisher;
+  image_transport::CameraSubscriber camera_subscriber_;
 
-    std::vector<std::vector<cv::Point2f>> corners, rejects; // rejects will likely be unused
-    cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
-    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
-    std::vector<int> MarkerIDs;
+  std::vector<std::vector<cv::Point2f>> corners, rejects;   // rejects will likely be unused
+  cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
+  cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(
+    cv::aruco::DICT_4X4_50);
+  std::vector<int> MarkerIDs;
 
-    int tagWidth; //actual tag width in cm
-    double width; //pixel width
-    int xCenter, yCenter;
-    double distance; //Returned distance in cm
-    double xAngle, yAngle; //Should be in degrees
+  int tagWidth;   //actual tag width in cm
+  double width;   //pixel width
+  int xCenter, yCenter;
+  double distance;   //Returned distance in cm
+  double xAngle, yAngle;   //Should be in degrees
 
-    const int numTags = 6; //There are 6 valid tags at the URC in 2023
-    int detectedTags[6]; //Keeps track of the tags that have already been detected in an image
+  const int numTags = 6;   //There are 6 valid tags at the URC in 2023
+  int detectedTags[6];   //Keeps track of the tags that have already been detected in an image
 
-    void imageCallback(
-        const sensor_msgs::msg::Image::ConstSharedPtr & image_msg,
-        const sensor_msgs::msg::CameraInfo::ConstSharedPtr & info_msg
-    );
+  void imageCallback(
+    const sensor_msgs::msg::Image::ConstSharedPtr & image_msg,
+    const sensor_msgs::msg::CameraInfo::ConstSharedPtr & info_msg
+  );
 };
 }
 
