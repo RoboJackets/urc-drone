d96addf95d9a0ad9e741491db0f3db5d24f12f9b
Kaylia1 <kaylia.mai@gmail.com>
2022-11-06 15:51:08 -0800

    math for lat long

--- urc_aruco/include/aruco_location.hpp
+++ urc_aruco/include/aruco_location.hpp
@@ -1,6 +1,7 @@
 #ifndef ARUCO_LOCATION_H
 #define ARUCO_LOCATION_H
 
+#include <vector>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_components/register_node_macro.hpp>
 #include <urc_msgs/msg/aruco_detection.hpp>
@@ -14,6 +15,11 @@ public:
     explicit ArucoLocation(const rclcpp::NodeOption & options);
 
 private:
+
+    double getNextLatitude(double d, double xAngle double yaw, double r);
+    double getNextLongitude(double d, double xAngle, double yaw, double r);
+    double findD(double trueD, double yAngle, double pitch);
+
     rclcpp::Publisher<urc_msgs::msg::ArucoLocation>::SharedPtr location_publisher;
     rclcpp::Subscription<urc_msgs::msg::ArucoDetection>::SharedPtr aruco_suscriber;
     rclcpp::Subscription<gps::fill_me_in_later::gps>::SharedPtr gps_suscriber; //TODO
--- urc_aruco/src/aruco_location.cpp
+++ urc_aruco/src/aruco_location.cpp
@@ -3,10 +3,40 @@
 namespace aruco_location
 {
     
-ArucoDetector::ArucoDetector(const rclcpp::NodeOptions & options)
+ArucoLocation::ArucoLocation(const rclcpp::NodeOptions & options)
 : rclcpp::Node("aruco_location", options)
 {
-    
+
+  location_publisher = create_publisher<urc_msgs::msg::ArucoLocation>(
+    "~/aruco",
+    rclcpp::SystemDefaultsQoS()
+  );
+
+  
+  aruco_subscriber = create_subscription<urc_msgs::msg::ArucoLocation>(
+    "~/aruco", rclcpp::SystemDefaultsQoS(), [this](const urc_msgs::msg::ArucoLocation msg) {
+      arucoCallback(msg);
+    });
+  gps_suscriber = create_subscription<urc_msgs::msg::ArucoLocation>(
+    "~/aruco", rclcpp::SystemDefaultsQoS(), [this](const urc_msgs::msg::ArucoLocation msg) {
+      gpsSubscriber(msg);
+    });
+  orientation_subscriber = create_subscription<urc_msgs::msg::ArucoLocation>(
+    "~/aruco", rclcpp::SystemDefaultsQoS(), [this](const urc_msgs::msg::ArucoLocation msg) {
+      orientationSubscriber(msg);
+    });
+}
+
+double ArucoLocation::getNextLatitude(double d, double xAngle, double yaw, double r) {
+  return asin(sin(droneLat)*cos(d / r) + cos(droneLat)*sin(d/r)*cos(xAngle + yaw));
+}
+
+double ArucoLocation::getNextLongitude(double d, double xAngle, double yaw, double r) { //wouldn't this be xAngle+yaw
+  return droneLon + atan2(sin(xAngle + yaw)*sin(d/r)*cos(droneLat), cos(d/R)-sin(droneLat)*sin(getNextLatitude(d, xAngle, yaw, r))); //er fix this extra call
+}
+
+double findD(double trueD, double yAngle, double pitch){
+  return trueD * cos(pitch + yAngle);
 }
 
 }
