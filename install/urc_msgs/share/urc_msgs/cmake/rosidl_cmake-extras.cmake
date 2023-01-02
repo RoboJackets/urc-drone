facf8fca7fd641adad48702c303386b55f0b5d8d
a-stickan <aidan.stickan@gmail.com>
2023-01-02 16:48:03 -0500

    Minor CI change, removal of build files

--- install/urc_msgs/share/urc_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(urc_msgs_IDL_FILES "msg/ArucoDetection.idl;msg/ArucoLocation.idl")
-set(urc_msgs_INTERFACE_FILES "msg/ArucoDetection.msg;msg/ArucoLocation.msg")
