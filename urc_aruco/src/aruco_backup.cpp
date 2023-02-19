8232b9322c57503f3a641c121c3d929efec98ccb
Ansh Gandhi <ansh.goodboy@gmail.com>
2023-02-19 16:42:30 -0500

    more blueprint work

--- urc_aruco/src/aruco_backup.cpp
+++ urc_aruco/src/aruco_backup.cpp
@@ -6,11 +6,11 @@ namespace aruco_backup
 ArucoBackup::ArucoBackup(const rclcpp::NodeOptions & options)
 : rclcpp::Node("aruco_backup", options)
 {
-  _cmd_publisher = create_publisher<urc_msgs::msg::VelocityPair>(
-    "~/motors",
+  _waypoint_pub = create_publisher<urc_msgs::msg::GPSLocation>(
+    "~/backup_waypoints",
     rclcpp::SystemDefaultsQoS());
 
-  _joy_subscriber = create_subscription<sensor_msgs::msg::Joy>(
+  _pose_sub = create_subscription<sensor_msgs::msg::Joy>(
     "~/joy", rclcpp::SystemDefaultsQoS(), [this](const sensor_msgs::msg::Joy msg) {
       joyCallback(msg);
     });
@@ -19,21 +19,10 @@ ArucoBackup::ArucoBackup(const rclcpp::NodeOptions & options)
   updater_ptr->setHardwareID("ck_driverck");
   updater_ptr->add("Joystick Diagnostic", this, &ArucoBackup::joystick_diagnostic);
 
-  absoluteMaxVel = declare_parameter<double>("absoluteMaxVel");
-  maxVel = declare_parameter<double>("maxVel");
-  maxVelIncr = declare_parameter<double>("maxVelIncr");
   leftJoyAxis = declare_parameter<int>("leftJoyAxis");
   rightJoyAxis = declare_parameter<int>("rightJoyAxis");
   leftInverted = declare_parameter<bool>("leftInverted");
   rightInverted = declare_parameter<bool>("rightInverted");
-}
-
-void ArucoBackup::joystick_diagnostic(diagnostic_updater::DiagnosticStatusWrapper & stat)
-{
-  stat.summary(diagnostic_msgs::msg::DiagnosticStatus::OK, "Joystick Online");
-  stat.add("absolute_max_velocity", absoluteMaxVel);
-  stat.add("max_velocity", maxVel);
-  stat.add("max_velocity_increment", maxVelIncr);
 }
 
 void ArucoBackup::joyCallback(const sensor_msgs::msg::Joy & msg)
@@ -54,7 +43,7 @@ void ArucoBackup::joyCallback(const sensor_msgs::msg::Joy & msg)
   cmd.right_velocity = msg.axes[rightJoyAxis] * maxVel * (rightInverted ? -1.0 : 1.0);
   cmd.header.stamp = this->get_clock()->now();
 
-  _cmd_publisher->publish(cmd);
+  _waypoint_pub->publish(cmd);
 }
 }
 
